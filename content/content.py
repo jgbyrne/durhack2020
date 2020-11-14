dd7a118b1fc54642ee240749ed636786079e3de9
Jack Byrne <jack.byrne@durham.ac.uk>
2020-11-14 23:11:46 +0000

    Images!

--- content/content.py
+++ content/content.py
@@ -2,11 +2,22 @@ import music
 
 import sqlite3
 import uuid
+import io
 
-from flask import Flask, make_response, jsonify, request
+from flask import Flask, make_response, jsonify, request, send_file
 app = Flask(__name__)
 
 music_items = music.Music("./music")
+
+# When a search is performed, an item_set is produced
+# These items are then cached in two variables
+
+# temp_items = {temp_id : (item, set_id)}
+# temp_item_sets = {set_id : (item_type, item_set)}
+
+# When one item is retrieved from the temporary set,
+# it is made permanant (retaining its temp_id); the other
+# temporary items from the same set are all discarded.
 
 temp_items = {}
 temp_item_sets = {}
@@ -55,6 +66,7 @@ def get_item(item_id):
         for item_id in item_set:
             del temp_items[item_id]
         del temp_item_sets[set_id]
+
         if item_type == "album":
             album = music_items.get_album(item["album_id"])
             values = (item["id"], "album", album["album_id"])
@@ -73,4 +85,33 @@ def get_item(item_id):
             return make_response(jsonify({"msg": "No such Item Type"}), 400)
     else:
         return make_response(jsonify({"msg": "Not Found"}), 404)
+
+@app.route('/item/<string:item_id>/image', methods=['GET'])
+def get_image(item_id):
+    conn = sqlite3.connect("items.db")
+    c = conn.cursor()
+    c.execute("SELECT * FROM items WHERE item_id = ?", (item_id,))
+    if item := c.fetchone():
+        _, item_type, impl_id = item
+        if item_type == "album":
+            buf = music_items.get_album_art(impl_id)
+            return send_file(io.BytesIO(buf),
+                             mimetype='image/png')
+        return make_response(jsonify({"msg": "No such Item Type"}), 400)
+    return make_response(jsonify({"msg": "Not Found"}), 404)
+
+@app.route('/item/<string:item_id>/thumbnail', methods=['GET'])
+def get_thumbnail(item_id):
+    conn = sqlite3.connect("items.db")
+    c = conn.cursor()
+    c.execute("SELECT * FROM items WHERE item_id = ?", (item_id,))
+    if item := c.fetchone():
+        _, item_type, impl_id = item
+        if item_type == "album":
+            buf = music_items.get_album_thumbnail(impl_id)
+            return send_file(io.BytesIO(buf),
+                             mimetype='image/png')
+        return make_response(jsonify({"msg": "No such Item Type"}), 400)
+    return make_response(jsonify({"msg": "Not Found"}), 404)
+
 
