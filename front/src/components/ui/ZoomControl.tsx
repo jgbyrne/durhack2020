0d81b43434b6d2f2e4ac90dc0b99e40390e90d77
Jamie-Stirling <stirlingj00@gmail.com>
2020-11-15 07:27:38 +0000

    added zoom control doesnt work

---
+++ front/src/components/ui/ZoomControl.tsx
@@ -0,0 +1,10 @@
+// @ts-ignore
+import React, {FC} from "react";
+import "./ZoomControl.css";
+
+const ZoomControl: FC = () => {
+    return <div className="ZoomControl">
+    </div>
+};
+
+export default ZoomControl;
