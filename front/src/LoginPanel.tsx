bc9359c5314cf87967e7287673a04636344792c8
Kacper Lubisz <kacperlubisz@gmail.com>
2020-11-15 01:06:47 +0000

    Implemented simple login

--- front/src/LoginPanel.tsx
+++ front/src/LoginPanel.tsx
@@ -1,7 +1,16 @@
-import React, {FC} from "react";
+import React, {FC, useContext, useState} from "react";
+import {LoginContext} from "./LoginContext";
 
 export const LoginPanel: FC<unknown> = () => {
 
-    return <>Please login</>
+    const [username, setUsername] = useState<string>("")
+    const login = useContext(LoginContext)
+
+    return <>
+        <form onSubmit={e => e.preventDefault()}>
+            <input type="text" onChange={e => setUsername(e.target.value)} value={username}/>
+            <button type="button" onClick={() => login?.login(username)}>Login</button>
+        </form>
+    </>
 
 }
