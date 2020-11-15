304662eea6f36a4866b2d820dc9f85b235ec8269
Jamie-Stirling <stirlingj00@gmail.com>
2020-11-15 01:57:49 +0000

    made styling better and added search icon

--- front/src/components/ui/SearchBar.tsx
+++ front/src/components/ui/SearchBar.tsx
@@ -1,9 +1,11 @@
 // @ts-ignore
 import React, {FC} from "react";
 import "./SearchBar.css";
+import searchIcon from "./img/search-icon.svg";
 
 const SearchBar: FC = () => {
     return <div className="SearchBar">
+        <img src={searchIcon}/>
     </div>
 };
 
