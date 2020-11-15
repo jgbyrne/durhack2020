0ee8a97bf1f496d6e79ae365479a2319603747e8
Jamie-Stirling <stirlingj00@gmail.com>
2020-11-15 01:12:25 +0000

    made brand better

--- front/src/components/ui/Brand.tsx
+++ front/src/components/ui/Brand.tsx
@@ -1,9 +1,11 @@
 // @ts-ignore
 import React, {FC} from "react";
 import "./Brand.css"
+import flowormLogo from "./img/floworm-logo-blue.svg";
 
 const Brand: FC = () => {
     return <div className="Brand">
+        <img src={flowormLogo}/>
     </div>
 };
 
